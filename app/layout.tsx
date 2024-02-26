561d259485ef72e659f766c0c1740011291e0d34
Michael <kontakt@ozum.pl>
2024-02-27 00:36:24 +0100

    feat(layout): Add ClerkProvider component to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const spaceGrotesk = Space_Grotesk({
   weight: ["400", "500", "600", "700"],
@@ -20,12 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn("font-spaceGrotesk antialiased", spaceGrotesk.variable)} // cn is to merge a couple of class names together
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={cn("font-spaceGrotesk antialiased", spaceGrotesk.variable)} // cn is to merge a couple of class names together
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
