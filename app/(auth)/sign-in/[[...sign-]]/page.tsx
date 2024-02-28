47584a0164b11cdc84afc6db04547718820c709d
Michael <kontakt@ozum.pl>
2024-02-28 01:36:40 +0100

    feat: Add sign-in and sign-up pages with Clerk components.

---
+++ app/(auth)/sign-in/[[...sign-]]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SignIn } from "@clerk/nextjs";
+
+const SignInPage = () => {
+  return <SignIn />;
+};
+
+export default SignInPage;
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+  return <SignUp />;
+};
+
+export default SignUpPage;
