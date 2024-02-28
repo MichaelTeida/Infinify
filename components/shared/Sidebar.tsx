4659d9a38d8d4bed01b523f9d472c3fcdcb97a11
Michael <kontakt@ozum.pl>
2024-02-28 23:17:41 +0100

    style: update authentication background color to bg-orange-50 and adjust sidebar logo size in Sidebar component (1)

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
-      <div className="sidebar-container"></div>
+      <div className="sidebar-container">
+        <Link href="/" className="sidebar-logo">
+          <Image
+            src="/assets/images/LOGO Infinify text 329x83.webp"
+            alt="Logo"
+            width={190}
+            height={50}
+          />
+        </Link>
+      </div>
     </aside>
   );
 };
