460a3fa6564156918c316f4f315fbf726a11a5df
Michael <kontakt@ozum.pl>
2024-02-28 22:56:23 +0100

    feat: add Sidebar component to Layout and CSS styles for Sidebar.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Sidebar from "@/components/shared/Sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
