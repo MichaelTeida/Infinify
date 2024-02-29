18d71b47010938e0dc7b7c75db523838cd5423d4
Michael <kontakt@ozum.pl>
2024-02-29 01:04:08 +0100

    style: add navigation links to the sidebar dynamically based on active route

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { SignedIn } from "@clerk/nextjs";
+import { navLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="sidebar">
       <div className="sidebar-container">
@@ -17,7 +23,20 @@ const Sidebar = () => {
         </Link>
         <nav className="sidebar-nav">
           <SignedIn>
-            <ul className="sidebar-list"></ul>
+            <ul className="sidebar-list">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname;
+
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-element group ${isActive ? "bg-orange-500 text-white" : "text-gray-800"}`}
+                  >
+                    {link.label}
+                  </li>
+                );
+              })}
+            </ul>
           </SignedIn>
         </nav>
       </div>
