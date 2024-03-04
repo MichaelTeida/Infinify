9017a370a2a3fc3381d2bc9e48db2fabd563188b
Michael <kontakt@ozum.pl>
2024-03-04 02:29:10 +0100

    feat: add NavMobile component to layout component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "@/components/shared/Sidebar";
+import NavMobile from "@/components/shared/NavMobile";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
+      <NavMobile />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
