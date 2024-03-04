f81913a042564ae0128c70a073fc4e0d81fb40be
Michael <kontakt@ozum.pl>
2024-03-04 23:14:21 +0100

    feat(database): add Mongoose connection setup function

---
+++ lib/database/mongoose.tsx
@@ -0,0 +1,52 @@
+import mongoose, { Mongoose } from "mongoose";
+
+// URL to MongoDB database
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null; // connection
+  promise: Promise<Mongoose> | null;
+}
+
+// Cache for MongoDB connection
+let cached: MongooseConnection = (global as any).mongoose;
+
+// Initialize cache if it doesn't exist
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+// Function to establish database connection
+export const connectToDatabase = async () => {
+  // If connection already exists, return it
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  // If database URL is not available, throw an error
+  if (!MONGODB_URL) {
+    throw new Error("MONGODB_URL not found");
+  }
+
+  try {
+    // Establishing connection to the database
+    cached.promise =
+      cached.promise ||
+      mongoose.connect(MONGODB_URL, {
+        dbName: "Infinify",
+        bufferCommands: false,
+      });
+
+    // Waiting for the connection
+    cached.conn = await cached.promise;
+  } catch (error) {
+    // If connection fails, throw an error
+    throw new Error("Failed to connect to MongoDB: " + error);
+  }
+
+  // Return the connection
+  return cached.conn;
+};
