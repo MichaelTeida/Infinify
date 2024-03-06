0e7d3e35e765ad8476e7dc1c31c88364693b4ce1
Michael <kontakt@ozum.pl>
2024-03-06 19:54:29 +0100

    refactor: update SVG gradient colors and add URL parameter function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { parse, stringify } from "qs";
 
 // Function for merging CSS classes
 export function cn(...inputs: ClassValue[]) {
@@ -19,15 +20,17 @@ export const handleError = (err: unknown) => {
 
 // Function for creating an SVG image
 const createSvg = (width: number, height: number) => `
-<svg width="${width}" height="${height}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#7986AC" offset="20%" />
-      <stop stop-color="#68769e" offset="50%" />
-      <stop stop-color="#7986AC" offset="70%" />
+      <stop stop-color="#FFA500" offset="10%" />
+      <stop stop-color="#FF8C00" offset="30%" />
+      <stop stop-color="#FFA500" offset="50%" />
+      <stop stop-color="#FF8C00" offset="70%" />
+      <stop stop-color="#FFA500" offset="90%" />
     </linearGradient>
   </defs>
-  <rect width="${width}" height="${height}" fill="#7986AC" />
+  <rect width="${width}" height="${height}" fill="#FFA500" />
   <rect id="r" width="${width}" height="${height}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${width}" to="${width}" dur="1s" repeatCount="indefinite"  />
 </svg>`;
@@ -42,3 +45,21 @@ const encodeToBase64 = (input: string) =>
 export const dataUrl = `data:image/svg+xml;base64,${encodeToBase64(
   createSvg(1000, 1000),
 )}`;
+
+// Function for creating a URL with search parameters
+export const formUrlQuery = ({
+  searchParams,
+  key,
+  value,
+}: FormUrlQueryParams) => {
+  // Parsing existing search parameters and adding a new one or updating an existing one
+  const params = { ...parse(searchParams.toString()), [key]: value };
+  // Returning a new URL with updated search parameters
+  return `${window.location.pathname}?${stringify(params, { skipNulls: true })}`;
+};
+
+type FormUrlQueryParams = {
+  searchParams: URLSearchParams;
+  key: string;
+  value: string;
+};
