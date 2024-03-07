9c5d62d4fafb9d99440373eba563bf16b85af40d
Michael <kontakt@ozum.pl>
2024-03-08 00:26:49 +0100

    test(api): handle error when creating user in clerk webhook route and fix username 3

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username || "",
+      username: username || "Anonymous",
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
