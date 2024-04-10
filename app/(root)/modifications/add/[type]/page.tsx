dc0e86440e3081a79719907f9e6bb07f79d43e35
Michael <kontakt@ozum.pl>
2024-04-10 22:42:29 +0200

    fix: fix error `TS7053`

--- app/(root)/modifications/add/[type]/page.tsx
+++ app/(root)/modifications/add/[type]/page.tsx
@@ -3,7 +3,8 @@ import Header from "@/components/shared/Header";
 import { modificationTypes } from "@/constants";
 
 const AddModificationTypePage = ({ params: { type } }: SearchParamProps) => {
-  const modification = modificationTypes[type];
+  const modification =
+    modificationTypes[type as keyof typeof modificationTypes];
 
   return (
     <Header title={modification.title} description={modification.description} />
