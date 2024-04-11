9555db62a30bbc20cdc791cca4b63c7d721ed18b
Michael <kontakt@ozum.pl>
2024-04-11 21:22:43 +0200

    feat: add ModificationForm component for adding modifications.

--- app/(root)/modifications/add/[type]/page.tsx
+++ app/(root)/modifications/add/[type]/page.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Header from "@/components/shared/Header";
 import { modificationTypes } from "@/constants";
+import ModificationForm from "@/components/shared/ModificationForm";
 
 const AddModificationTypePage = ({ params: { type } }: SearchParamProps) => {
   const modification =
     modificationTypes[type as keyof typeof modificationTypes];
 
   return (
-    <Header title={modification.title} description={modification.description} />
+    <>
+      <Header
+        title={modification.title}
+        description={modification.description}
+      />
+      <ModificationForm />
+    </>
   );
 };
 
---
+++ components/shared/ModificationForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ModificationForm = () => {
+  return <div>ModificationForm</div>;
+};
+
+export default ModificationForm;
