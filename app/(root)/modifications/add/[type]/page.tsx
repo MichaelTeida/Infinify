7363947e8d5abfadca7cafe2d61753b4901780b7
Michael <kontakt@ozum.pl>
2024-04-11 23:59:59 +0200

    feat: add async/await functionality to handle user authentication and redirection in AddModificationTypePage

--- app/(root)/modifications/add/[type]/page.tsx
+++ app/(root)/modifications/add/[type]/page.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "@/components/shared/Header";
 import { modificationTypes } from "@/constants";
 import ModificationForm from "@/components/shared/ModificationForm";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs";
 
-const AddModificationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddModificationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
   const modification =
     modificationTypes[type as keyof typeof modificationTypes];
-  const { userId } = auth;
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
 
   return (
     <>
@@ -15,7 +23,12 @@ const AddModificationTypePage = ({ params: { type } }: SearchParamProps) => {
         title={modification.title}
         description={modification.description}
       />
-      <ModificationForm action="Add" userId={} />
+      <ModificationForm
+        action="Add"
+        userId={user._id}
+        type={modification.type as ModificationTypeKey}
+        tokenBalance={user.tokenBalance}
+      />
     </>
   );
 };
