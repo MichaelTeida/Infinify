46a235f066c07c54a2829dc3ae8031cf054172a3
Michael <kontakt@ozum.pl>
2024-04-14 10:44:46 +0200

    feat(constants): add aspect ratio options for images

--- constants/index.ts
+++ constants/index.ts
@@ -124,3 +124,30 @@ export const defaultValues = {
   prompt: "",
   publicId: "",
 };
+
+export const aspectRatioOptions = {
+  "1:1": {
+    aspectRatio: "1:1",
+    label: "Square (1:1)",
+    width: 1000,
+    height: 1000,
+  },
+  "16:9": {
+    aspectRatio: "16:9",
+    label: "Desktop format (16:9)",
+    width: 1920,
+    height: 1080,
+  },
+  "3:4": {
+    aspectRatio: "3:4",
+    label: "Standard Portrait (3:4)",
+    width: 1000,
+    height: 1334,
+  },
+  "9:16": {
+    aspectRatio: "9:16",
+    label: "Phone Portrait (9:16)",
+    width: 1000,
+    height: 1778,
+  },
+};
