a7f02bc0e4d6a8823164a39652c9c2ba8d480ad4
Michael <kontakt@ozum.pl>
2024-06-13 00:14:35 +0200

    feat: add debounce

--- lib/utils.ts
+++ lib/utils.ts
@@ -74,3 +74,12 @@ export const getImageSize = (
   }
   return image?.[dimension] || 1000;
 };
+
+// DEBOUNCE
+export const debounce = (func: (...args: any[]) => void, delay: number) => {
+  let timeoutId: NodeJS.Timeout | null;
+  return (...args: any[]) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(null, args), delay);
+  };
+};
