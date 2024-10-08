a87c5213146544409f07b0628c65423fc186fde5
Michael <kontakt@ozum.pl>
2024-10-08 11:03:40 +0200

    feat: add Toast from shadcn/ui

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,9 @@
+import { useToast } from "@/hooks/use-toast";
+
+const MediaUploader = () => {
+  const { toast } = useToast();
+
+  return <div>MediaUploader</div>;
+};
+
+export default MediaUploader;
