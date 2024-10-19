dfe131255f4f0175e3e20fad530688a9df1d1dff
Michael <kontakt@ozum.pl>
2024-10-19 17:55:50 +0200

    refactor(root): add Toaster

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "@/components/shared/Sidebar";
 import NavMobile from "@/components/shared/NavMobile";
+import { Toaster } from "@/components/ui/toaster";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -10,6 +11,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
+      <Toaster />
     </main>
   );
 };
