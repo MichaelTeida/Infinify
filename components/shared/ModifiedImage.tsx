ad6858fc164a28edbc6bcb1a1dfeef5a945fd3bd
Michael <kontakt@ozum.pl>
2024-11-13 19:30:47 +0100

    feat(image): implement image downloading functionality + toaster

--- components/shared/ModifiedImage.tsx
+++ components/shared/ModifiedImage.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import DownloadIcon from "@mui/icons-material/Download";
 
-import { CldImage } from "next-cloudinary";
-import { dataUrl, debounce, getImageSize } from "@/lib/utils";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import SyncIcon from "@mui/icons-material/Sync";
+import { useToast } from "@/hooks/use-toast";
 
 const ModifiedImage = ({
   image,
@@ -17,7 +18,40 @@ const ModifiedImage = ({
   modificationConfig,
   hasDownload = image?.publicId && modificationConfig && !isModifying,
 }: ModifiedImageProps) => {
-  const downloadHandler = () => {};
+  const { toast } = useToast();
+  const [isDownloading, setIsDownloading] = useState(false);
+
+  const downloadHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+
+    if (isDownloading) {
+      toast({
+        title: "Please wait",
+        description: "You can only download once every 5 seconds.",
+        duration: 3500,
+        className: "media-error-toast",
+      });
+      return;
+    }
+
+    setIsDownloading(true);
+
+    download(
+      getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...modificationConfig,
+      }),
+      title,
+    );
+
+    setTimeout(() => {
+      setIsDownloading(false);
+    }, 5000);
+  };
 
   return (
     <div className="flex flex-col gap-4">
@@ -26,7 +60,7 @@ const ModifiedImage = ({
 
         {hasDownload && (
           <button
-            onClick={() => downloadHandler}
+            onClick={downloadHandler}
             className="flex items-center md:px-5 gap-2 font-semibold text-dark-500"
           >
             <DownloadIcon className="image-download_icon" /> Download
