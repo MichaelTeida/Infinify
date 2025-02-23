e1ce9bcf3ed324a6eff9daf927d94ff448ee9482
Michael <kontakt@ozum.pl>
2025-02-23 23:30:13 +0100

    feat(chat): Implement chat page UI and functionality

--- app/(root)/chat/page.tsx
+++ app/(root)/chat/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Header from "@/components/shared/Header";
+import Chat from "@/components/shared/Chat";
+import { redirect } from "next/navigation";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
 
-const Chat = () => {
-  return <div>Chat</div>;
+const ChatPage = async () => {
+  const authResponse = await auth(); // Await the auth() call
+  if (!authResponse?.userId) redirect("/sign-in");
+  const user = await getUserById(authResponse.userId);
+
+  return (
+    <>
+      <Header
+        title={"Chat AI"}
+        description={
+          "Engage in interactive conversations powered by artificial intelligence."
+        }
+      />
+      <section className="mt-8">
+        <Chat tokenBalance={user.tokenBalance} />
+      </section>
+    </>
+  );
 };
 
-export default Chat;
+export default ChatPage;
